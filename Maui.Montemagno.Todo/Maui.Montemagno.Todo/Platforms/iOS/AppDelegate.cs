5ade070d1c9007b18c514301944457600c3d72f8
paguirre82 <pablod@teambaw.com.au>
2022-08-18 10:58:54 +1000

    initial commit

---
+++ Maui.Montemagno.Todo/Maui.Montemagno.Todo/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Maui.Montemagno.Todo;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
